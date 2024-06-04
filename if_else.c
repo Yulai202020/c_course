e485487c794a01c2f7c6504f343ff90e873c7e84
Yulai202020 <ngmyulai@gmail.com>
2024-06-04 10:54:29 +0400

    first commit

--- if_else.c
+++ if_else.c
@@ -7,15 +7,12 @@ int main() {
     scanf("%d", &age);
 
     if (age >= 18){
-        printf("You are aldut.\n");
+        printf("You are an aldut.\n");
     } else if (age <= 12) {
         printf("You are teenager.\n");
     } else {
         printf("You are baby.\n");
     }
-
-
-
 
     // use:
     // if (condition1) {
